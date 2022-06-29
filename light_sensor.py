9dae6027c7b296225928db6b56b03ba47138e25e
neverGonnaGiveUup123 <102629343+neverGonnaGiveUup123@users.noreply.github.com>
2022-06-29 12:47:51 +1200

    Update light_sensor.py

--- light_sensor.py
+++ light_sensor.py
@@ -14,12 +14,22 @@ def light_detector(gray):
 while True:
     check, frame = video.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    print(light_detector(gray))
+    print(int(light_detector(gray)))
     time.sleep(1)
     cv2.imshow("capturing",gray)
+    left_screen = numpy.mean(gray[220][0:300])
+    right_screen = numpy.mean(gray[-220][-0:-300])
 
     key = cv2.waitKey(1000)
     if key == ord('q'):
         break
+
+    elif left_screen > right_screen:
+        print("Rotate left")
+    elif right_screen > left_screen:
+        print("Rotate right")
+    else:
+        print("Error")
+
 video.release()
 cv2.destroyAllWindows()
