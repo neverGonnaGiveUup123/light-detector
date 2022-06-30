d711215cc6b6937ba2aae53c878a2789d2064c32
neverGonnaGiveUup123 <102629343+neverGonnaGiveUup123@users.noreply.github.com>
2022-06-30 13:58:30 +1200

    Increased accuracy
    Code measures light more accurately through increasing the number of lines it computes.

--- light_sensor.py
+++ light_sensor.py
@@ -4,32 +4,41 @@ import numpy
 
 video = cv2.VideoCapture(0)
 
-
 def light_detector(gray):
-    global light
-    light = 0
-    for i in gray:
-        light = light + numpy.mean(i)
-    return light // 1000
+    var1 = numpy.mean(gray[220][0:300])
+    var2 = numpy.mean(gray[0][0:300])
+    var3 = numpy.mean(gray[440][0:300])
+    var_1 = numpy.mean(gray[-220][-0:-300])
+    var_2 = numpy.mean(gray[-0][-0:-300])
+    var_3 = numpy.mean(gray[-440][-0:-300])
+    global right_screen
+    global left_screen
+    right_screen = var1 + var2 + var3 // 3
+    left_screen = var_1 + var_2 + var_3 // 3
+    return right_screen, left_screen
+
+def rotate_right():
+    if right_screen > left_screen:
+        print("Rotate right")
+def rotate_left():
+    if left_screen > right_screen:
+        print("Rotate left")
+
 while True:
     check, frame = video.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    print(int(light_detector(gray)))
-    time.sleep(1)
+    light_detector(gray)
+    time.sleep(3)
+    print(f"Left screen brightness: {int(left_screen)}")
+    print(f"Right screen brightness: {int(right_screen)}")
     cv2.imshow("capturing",gray)
-    left_screen = numpy.mean(gray[220][0:300])
-    right_screen = numpy.mean(gray[-220][-0:-300])
+
+    rotate_right()
+    rotate_left()
 
     key = cv2.waitKey(1000)
     if key == ord('q'):
         break
-
-    elif left_screen > right_screen:
-        print("Rotate left")
-    elif right_screen > left_screen:
-        print("Rotate right")
-    else:
-        print("Error")
 
 video.release()
 cv2.destroyAllWindows()
